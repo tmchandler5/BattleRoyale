86529238b82ab466d2b08d33b6d30df0812ab5e2
tmchandler5 <tmchandler5@gmail.com>
2015-09-30 21:43:30 -0500

    Initial Commit

---
+++ BattleRoyale/Character Resources/Character.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleRoyale.Character_Resources
+{
+    class Character
+    {
+        public int Health { get; protected set; }
+        public int Strength { get; set; }
+        public int Armor { get; set; }
+
+
+        public Character()
+        {
+            Health = 1000;
+        }
+
+        public int receiveDamage(int rawDmg)
+        {
+            double recDmg = rawDmg - (((Armor * 2.0) / 100.0) * rawDmg);
+            Health = Health - (int)recDmg;
+            return (int)recDmg;
+        }
+
+        public int doDamage()
+        {
+            int doDmg = (Strength * 2) + new Random().Next(50, 101);
+            return doDmg;
+        }
+
+
+    }
+}
---
+++ BattleRoyale/Character Resources/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleRoyale.Character_Resources
+{
+    class Player : Character
+    {
+        public Player()
+        {
+            Random rnd = new Random();
+            Strength = rnd.Next(5, 11);
+            System.Threading.Thread.Sleep(100);
+            Armor = rnd.Next(5, 11);
+        }
+
+
+    }
+}
