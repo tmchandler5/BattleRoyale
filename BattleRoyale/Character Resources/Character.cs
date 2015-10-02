fbc44792ba7e33e0f76a485f0fc5dac08bf6bd52
tmchandler5 <tmchandler5@gmail.com>
2015-10-01 21:35:52 -0500

    Update for attack total and defence total
    added an attack total and defense total to change from "receive damage"
    to "deal damage"

--- BattleRoyale/Character Resources/Character.cs
+++ BattleRoyale/Character Resources/Character.cs
@@ -18,19 +18,37 @@ namespace BattleRoyale.Character_Resources
             Health = 1000;
         }
 
-        public int receiveDamage(int rawDmg)
+        //public int receiveDamage(int rawDmg)
+        //{
+        //    double recDmg = rawDmg - (((Armor * 2.0) / 100.0) * rawDmg);
+        //    Health = Health - (int)recDmg;
+        //    return (int)recDmg;
+        //}
+
+
+        //public int doDamage()
+        //{
+        //    int doDmg = (Strength * 2) + new Random().Next(50, 101);
+        //    return doDmg;
+        //}
+
+        public int defenceTotal()
         {
-            double recDmg = rawDmg - (((Armor * 2.0) / 100.0) * rawDmg);
-            Health = Health - (int)recDmg;
-            return (int)recDmg;
+            int defTtl = (Armor * 2);
+            return defTtl;
+        }
+
+        public int attackTotal()
+        {
+            int atkTtl = (Strength * 2) + new Random().Next(60, 101);
+            return atkTtl;
         }
 
         public int doDamage()
         {
-            int doDmg = (Strength * 2) + new Random().Next(50, 101);
-            return doDmg;
+            int dealDmg = attackTotal() - defenceTotal();
+            return dealDmg;
         }
-
 
     }
 }
--- BattleRoyale/Program.cs
+++ BattleRoyale/Program.cs
@@ -15,20 +15,32 @@ namespace BattleRoyale
             System.Threading.Thread.Sleep(100);
             Player enemy1 = new Player();
 
-            while (player1.Health >= 0 && enemy1.Health >= 0)
-            {
-                Console.WriteLine("Player 1 does " + enemy1.receiveDamage(player1.doDamage()) + " damage to Enemy1.");
-                Console.WriteLine("Enemy1 has " + enemy1.Health + " health.\n");
-               
+            //while (player1.Health >= 0 && enemy1.Health >= 0)
+            //{
+            //    Console.WriteLine("Player 1 does " + enemy1.receiveDamage(player1.doDamage()) + " damage to Enemy1.");
+            //    Console.WriteLine("Enemy1 has " + enemy1.Health + " health.\n");
 
-                if (enemy1.Health > 0)
-                {
-                    Console.WriteLine("Enemy 1 does " + player1.receiveDamage(enemy1.doDamage()) + " damage to Player1.");
-                    Console.WriteLine("Player1 has " + player1.Health + " health.\n");
-                    
-                }
+
+            //    if (enemy1.Health > 0)
+            //    {
+            //        Console.WriteLine("Enemy 1 does " + player1.receiveDamage(enemy1.doDamage()) + " damage to Player1.");
+            //        Console.WriteLine("Player1 has " + player1.Health + " health.\n");
+
+            //    }
+            //}
+            
+
+
+
+
+
+            {
+                Console.WriteLine(player1.attackTotal());
+                Console.WriteLine(enemy1.defenceTotal());
+                Console.WriteLine(player1.doDamage());
             }
-           
+            System.Threading.Thread.Sleep(99999);
         }
+
     }
 }
