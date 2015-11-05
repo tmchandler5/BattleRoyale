f556f4daf901c50a2dcf9fa127963f0d88697ef2
JDunn3 <jeremydunn123@gmail.com>
2015-11-04 20:39:02 -0600

    Homework 1
    Thomas completed all questions and worked on some other problems for
    practice.

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -21,6 +21,23 @@ namespace Homework1
 
             Console.WriteLine(A + B + C + D + E + F + G);
 
+            //1.2
+            string[] myArray = new string[]
+            {
+                "Sadie", "jumps", "over", "the", "slow", "white", "fox"
+            };
+
+            for (int x = 0; x < myArray.Count(); x++)
+            {
+                Console.Write(myArray[x]+ " ");
+                if(x == myArray.Count() - 1)
+                    Console.WriteLine();
+            }
+        
+
+
+
+
             //2
             for (int x = 1; x <= 10; x++)
             {
@@ -29,19 +46,41 @@ namespace Homework1
             }
 
             //3
-            for (int y = 0; y <= 100; y++) ;
+            for (int y = 0; y <= 100; y++)
             {
-
+                if (y % 2 == 0)
+                { }
+                else
+                    Console.WriteLine(y);
             }
 
+            //2.1
+            Console.WriteLine(problem2("My puppies"));
 
+            //4
+            Console.WriteLine("Please enter a string:");
+            Console.WriteLine(problem2(Console.ReadLine()));
 
+            Console.WriteLine("Please enter a integer:");
+            int inputOne = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter another integer:");
+            int inputTwo = Int32.Parse(Console.ReadLine());
 
-
-
+            Console.WriteLine(product1(inputOne, inputTwo));
 
 
             System.Threading.Thread.Sleep(999999);
         }
+        static string problem2(string input)
+        {
+            return input + " are the best.";
+        }
+
+        static int product1(int numOne, int numTwo)
+        {
+            return numOne * numTwo;
+        }
+        
+        
     }
 }
