f35d03c4a11f42b543ec1308644d9fe6fd4213ce
tmchandler5 <tmchandler5@gmail.com>
2015-11-04 19:02:07 -0600

    Homework1
    Added Homework1 project

---
+++ Homework1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            String A = "Sadie";
+            String B = " jumps";
+            String C = " over";
+            String D = " the";
+            String E = " slow";
+            String F = " white";
+            String G = " fox.";
+
+            Console.WriteLine(A + B + C + D + E + F + G);
+
+            //2
+            for (int x = 1; x <= 10; x++)
+            {
+                String numA = new String('A', x);
+                Console.WriteLine(numA);
+            }
+
+            //3
+            for (int y = 0; y <= 100; y++) ;
+            {
+
+            }
+
+
+
+
+
+
+
+
+            System.Threading.Thread.Sleep(999999);
+        }
+    }
+}
